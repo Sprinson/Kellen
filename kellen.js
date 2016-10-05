a3d14bedddfb43a3291e7258cbfa07c7ba57ac07
Chris Brinson <chrisjbrinson@gmail.com>
2016-10-04 22:36:15 -0600

    Initial commit

---
+++ kellen.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+  
+ 
+  $("#bob").hover(function(){
+    $(this).hide(1000);
+    $("#bob3").show(900);
+  });
+
+  $("#bob3").hover(function(){
+    $(this).hide(1000);
+    $("#bob2").show(900);
+  });
+
+  $("#bob2").hover(function(){
+    $(this).hide(1000);
+    $("#bob4").show(1500);
+  });
+
+  $("#bob4").hover(function(){
+    $(this).hide(1000);
+    $("#bob").show(900);
+  });
+
+  $("#bob2").hover(function(){
+    $(this).hide(1000);
+    $("#bob5").show(200);
+  });
+
+  $("#bob5").hover(function(){
+    $(this).hide(2000);
+    
+  });
+
+
+
+
+  
+
+
+
+ 
+  
+  
+  
+
+ 
+});
